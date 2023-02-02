12954364b6290cdf4bd007c50725b2e5f0c317ee
Miha Krajnc <miha.krajnc8@gmail.com>
2023-02-02 14:15:04 +0100

    Switch from glfw to SDL

--- CherryExample/src/example.cpp
+++ CherryExample/src/example.cpp
@@ -1,9 +1,10 @@
 #include <GL/glew.h>
-#include <GLFW/glfw3.h>
+#include <SDL3/SDL.h>
 int main() {
-	int r = glfwInit(); 
-	GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-	glfwMakeContextCurrent(window);
+	int r = SDL_Init(SDL_INIT_VIDEO);
+	SDL_Window* mainwindow = SDL_CreateWindow("CherryExample", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+		512, 512, SDL_WINDOW_OPENGL);
+	SDL_GLContext maincontext = SDL_GL_CreateContext(mainwindow);
 	GLenum res = glewInit();
 	return 0;
 }
